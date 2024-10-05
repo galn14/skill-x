79b27826f5d79ae1e38f421e83dbade0f234974b
AnantaMR <ananta.dika.mahardika@gmail.com>
2024-10-05 16:13:35 +0700

    tab3 progress

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar ,IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem,
+  IonLabel, IonList, IonThumbnail,IonSearchbar } from '@ionic/react';
+
+
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import './Tab3.css';  
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Skill X</IonTitle>
         </IonToolbar>
+
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+      <IonSearchbar></IonSearchbar>
+      
+      <IonCard > 
+        <IonItem>
+      
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" className='icon1'/>
+
+      <div className="text-container1">
+      <IonCardTitle class="ion-card-title-tab3">Building Website</IonCardTitle>
+      <IonCardSubtitle class="ion-card-subtitle-tab3">Aileen Angelica</IonCardSubtitle>
+      </div>
+      
+      <h1 className="status-label">Completed</h1>
+
+      </IonItem>
+      
+      <IonCardContent>
+        <IonList>
+          <IonItem>
+            
+              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" className='icon2'/>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+        </IonList>
+        </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
