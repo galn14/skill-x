d3615eee883cb50ac3936d52e9300da21c5f99b4
Galeno Areliano <galenofilberto@gmail.com>
2024-10-05 13:26:22 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'skill-x',
+  webDir: 'dist'
+};
+
+export default config;
