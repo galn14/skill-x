71d7621f5ca4ffe3fcfaff14a8a331075e19b2c1
aileenliexiuai <aileen.liexiuai@gmail.com>
2024-10-05 16:19:49 +0700

    tab4

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -1,25 +1,66 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList, IonItem, IonLabel, IonAvatar, IonImg, IonIcon } from '@ionic/react';
 import './Tab4.css';
+import { } from '@ionic/react';
+import { heartOutline } from 'ionicons/icons'; 
 
-const Tab4: React.FC = () => {
+
+const Account: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+
+    <IonHeader translucent={true} className="custom-header">
         <IonToolbar>
-          <IonTitle>Tab 4</IonTitle>
+        <IonButton className="close-button" slot="start" fill="clear">x</IonButton>
+        <IonTitle className="subtitle">Main Menu</IonTitle>
         </IonToolbar>
-      </IonHeader>
+    </IonHeader>
+
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 4</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Halo ges" />
+        {/* User Profile Section */}
+        <div className="profile-section">
+          <IonAvatar className="avatar">
+            <IonImg src="path_to_profile_image" alt="User Avatar" />
+          </IonAvatar>
+          <div className="user-details">
+            <h3>Ailin</h3>
+            <IonButton fill="outline" size="small">Join as a seller</IonButton>
+          </div>
+        </div>
+
+        {/* Promotion Section */}
+        <div className="promotion-section">
+          <h2>Earn badges and stand out</h2>
+          <p>Boost your sales, by boosting your expertise.</p>
+          <IonButton fill="solid" className="enroll">Enroll Now</IonButton>
+        </div>
+
+        {/* Menu List */}
+        <IonList>
+          <IonItem>
+            <IonLabel>Transaction List</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonIcon icon={heartOutline} slot="start" aria-label="Wishlist" /> 
+            <IonLabel>Wishlist</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Following Seller</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Review</IonLabel>
+          </IonItem>
+          <div className="custom-divider"></div>
+
+          <IonItem>
+            <IonLabel>Complained Order</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Help and Support</IonLabel>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab4;
+export default Account;
