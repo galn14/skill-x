3b28d3fc2376ba4fad96cc35216973c8f5d6b6a7
Galeno Areliano <galenofilberto@gmail.com>
2024-10-05 19:45:55 +0700

    Tab1-Swiper

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonIcon, IonCard, IonCardContent, IonButton, IonFooter, IonTabBar, IonTabButton, IonCardHeader, IonCardSubtitle, IonCardTitle, IonLabel } from '@ionic/react';
 import { notificationsOutline, mailOutline, menuOutline, desktopOutline, colorPaletteOutline, constructOutline, videocamOutline, brushOutline, globeOutline } from 'ionicons/icons';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper/modules';
+import { Autoplay, Grid, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper/modules';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -14,14 +14,16 @@ const Tab1: React.FC = () => {
     setCategories([
       { id: 1, name: 'Computer Science', icon: desktopOutline, link: '/category/computer-science' },
       { id: 2, name: 'Visual Communication Design', icon: colorPaletteOutline, link: '/category/visual-communication-design' },
-      { id: 3, name: 'Interior Design', icon: constructOutline, link: '/category/interior-design' }
+      { id: 3, name: 'Interior Design', icon: constructOutline, link: '/category/interior-design' },
+      { id: 4, name: 'DBI', icon: constructOutline, link: '/category/DBI' }
     ]);
 
     // Fetch popular services from the database
     setServices([
       { id: 1, name: 'Editing Video', icon: videocamOutline, link: '/service/editing-video' },
       { id: 2, name: 'Logo Design', icon: brushOutline, link: '/service/logo-design' },
-      { id: 3, name: 'Build Website', icon: globeOutline, link: '/service/build-website' }
+      { id: 3, name: 'Build Website', icon: globeOutline, link: '/service/build-website' },
+      { id: 4, name: 'Build Prototyoe', icon: globeOutline, link: '/service/build-prototype' }
     ]);
   }, []);
 
@@ -42,27 +44,53 @@ const Tab1: React.FC = () => {
           <h3>Hi Ailin, what are you looking for?</h3>
         </div>
 
+        
         <div className="categories">
-          <h3>Categories</h3>
-          <Swiper modules={[Keyboard, Pagination, Scrollbar, Zoom]} direction={'vertical'} keyboard={true} pagination={true} scrollbar={true} zoom={true} spaceBetween={20} slidesPerView={'auto'}>
-            {categories.map((category) => (
-              <SwiperSlide key={category.id}>
-                <IonCard button routerLink={category.link} className="category-card">
-                  <IonCardContent className="category-card-content">
-                    <IonIcon icon={category.icon} className="category-icon" />
-                    <IonLabel>{category.name}</IonLabel>
-                  </IonCardContent>
-                </IonCard>
-              </SwiperSlide>
+        <h3>Categories</h3>        
+        <Swiper
+          modules={[Grid, Keyboard, Pagination, Scrollbar, Zoom]}
+          direction="horizontal"
+          keyboard={true}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          zoom={true}
+          spaceBetween={10}
+          slidesPerView="auto"
+          freeMode={true}
+          grid={{ rows: 1 }} // Menetapkan jumlah baris grid menjadi 1
+        >
+          {categories.map((category) => (
+            <SwiperSlide
+              key={category.id}
+              style={{ width: 'auto', flexShrink: 0 }}
+              >
+              <IonCard button routerLink={category.link} className="category-card">
+                <IonCardContent className="category-card-content">
+                  <IonIcon icon={category.icon} className="category-icon" />
+                  <IonLabel>{category.name}</IonLabel>
+                </IonCardContent>
+              </IonCard>
+            </SwiperSlide>
             ))}
           </Swiper>
         </div>
 
         <div className="popular-services">
-          <h3>Popular services</h3>
-          <Swiper modules={[Keyboard, Pagination, Scrollbar, Zoom]} direction={'vertical'} keyboard={true} pagination={true} scrollbar={true} zoom={true} spaceBetween={20} slidesPerView={'auto'}>
+          <h3>Popular Services</h3>
+          <Swiper modules={[Grid, Keyboard, Pagination, Scrollbar, Zoom]} 
+          direction={'horizontal'} 
+          keyboard={true} 
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }} 
+          zoom={true} 
+          spaceBetween={10} 
+          slidesPerView={'auto'}
+          freeMode={true}
+          grid={{ rows: 1 }}>   
             {services.map((service) => (
-              <SwiperSlide key={service.id}>
+              <SwiperSlide key={service.id}
+              style={{ width: 'auto', flexShrink: 0 }}
+              >
                 <IonCard button routerLink={service.link} className="service-card">
                   <IonCardContent className="service-card-content">
                     <IonIcon icon={service.icon} className="service-icon" />
