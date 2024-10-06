99d3b09b284bfc589d27d540d1af390de4a2442b
AnantaMR <ananta.dika.mahardika@gmail.com>
2024-10-06 17:49:14 +0700

    tab 3 card completed

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar ,IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem,
-  IonLabel, IonList, IonThumbnail,IonSearchbar } from '@ionic/react';
+  IonLabel, IonIcon, IonList, IonThumbnail,IonSearchbar } from '@ionic/react';
 
 
 import ExploreContainer from '../components/ExploreContainer';
@@ -16,6 +16,8 @@ const Tab3: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
       <IonSearchbar></IonSearchbar>
+
+        {/* card  completed status */}
       
       <IonCard > 
         <IonItem>
@@ -24,23 +26,180 @@ const Tab3: React.FC = () => {
 
       <div className="text-container1">
       <IonCardTitle class="ion-card-title-tab3">Building Website</IonCardTitle>
-      <IonCardSubtitle class="ion-card-subtitle-tab3">Aileen Angelica</IonCardSubtitle>
+      <IonCardSubtitle class="ion-card-subtitle-tab3">AileenLiexiuai</IonCardSubtitle>
       </div>
       
       <h1 className="status-label">Completed</h1>
+      <IonButton className="kebab-icon"fill='clear' >
+      ︙
+    </IonButton>
 
       </IonItem>
       
       <IonCardContent>
-        <IonList>
-          <IonItem>
+        
+          <IonItem lines='none'>
             
               <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" className='icon2'/>
-            <IonLabel>Item</IonLabel>
+              <div  className='text-container2' >
+            <IonCardTitle class="ion-card-title2-tab3">Package Portofolio Website</IonCardTitle>
+            <IonCardSubtitle class="ion-card-subtitle-tab3">Computer Science</IonCardSubtitle>
+            
+              </div>
+              
           </IonItem>
-        </IonList>
+      
         </IonCardContent>
+          <IonItem lines="none" className="order-section">
+      <div className="order-details">
+        <p>Total Order</p>
+        <h2>Rp 500.000</h2>
+      </div>
+
+      {/* Buttons */}
+      <div className="button-group">
+        <IonButton size="small" fill="outline" className='custom-button-outline'>Buy Again</IonButton>
+        <IonButton size="small" className='custom-button'>Review</IonButton>
+      </div>
+    </IonItem>
         </IonCard>
+  {/* card  incomplete status */}
+        <IonCard > 
+
+        <IonItem>
+      
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" className='icon1'/>
+
+      <div className="text-container1">
+      <IonCardTitle class="ion-card-title-tab3">Photography</IonCardTitle>
+      <IonCardSubtitle class="ion-card-subtitle-tab3">GeLion</IonCardSubtitle>
+      </div>
+      
+      <h1 className="status-label">Incomplete</h1>
+      <IonButton className="kebab-icon"fill='clear' >
+      ︙
+    </IonButton>
+
+      </IonItem>
+      
+      <IonCardContent>
+        
+          <IonItem lines='none'>
+            
+              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" className='icon2'/>
+              <div  className='text-container2' >
+            <IonCardTitle class="ion-card-title2-tab3">Product Photography</IonCardTitle>
+            <IonCardSubtitle class="ion-card-subtitle-tab3">Visual Communication Design</IonCardSubtitle>
+            
+              </div>
+              
+          </IonItem>
+      
+        </IonCardContent>
+          <IonItem lines="none" className="order-section">
+      <div className="order-details">
+        <p>Total Order</p>
+        <h2>Rp 1.500.000</h2>
+      </div>
+
+      {/* Buttons */}
+      <div className="button-group">
+        <IonButton size="small" className='custom-button'>Contact Seller</IonButton>
+      </div>
+    </IonItem>
+        </IonCard>
+
+      {/* card pending order */}
+
+        <IonCard > 
+        <IonItem>
+      
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" className='icon1'/>
+
+      <div className="text-container1">
+      <IonCardTitle class="ion-card-title-tab3">Copy Writing</IonCardTitle>
+      <IonCardSubtitle class="ion-card-subtitle-tab3">Ananta</IonCardSubtitle>
+      </div>
+      
+      <h1 className="status-label">Awaiting conformation</h1>
+      <IonButton className="kebab-icon"fill='clear' >
+      ︙
+    </IonButton>
+
+      </IonItem>
+      
+      <IonCardContent>
+        
+          <IonItem lines='none'>
+            
+              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" className='icon2'/>
+              <div  className='text-container2' >
+            <IonCardTitle class="ion-card-title2-tab3">Package Script Video</IonCardTitle>
+            <IonCardSubtitle class="ion-card-subtitle-tab3">Communication</IonCardSubtitle>
+            
+              </div>
+              
+          </IonItem>
+      
+        </IonCardContent>
+          <IonItem lines="none" className="order-section">
+      <div className="order-details">
+        <p>Total Order</p>
+        <h2>Rp 700.000</h2>
+      </div>
+
+      {/* Buttons */}
+      <div className="button-group">
+        <IonButton size="small" fill="outline" className='custom-button-outline'>Request Refund</IonButton>
+        <IonButton size="small" className='custom-button'>complete</IonButton>
+      </div>
+    </IonItem>
+        </IonCard>
+        <IonCard > 
+        <IonItem>
+      
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" className='icon1'/>
+
+      <div className="text-container1">
+      <IonCardTitle class="ion-card-title-tab3">Building Website</IonCardTitle>
+      <IonCardSubtitle class="ion-card-subtitle-tab3">AileenLiexiuai</IonCardSubtitle>
+      </div>
+      
+      <h1 className="status-label">Completed</h1>
+      <IonButton className="kebab-icon"fill='clear' >
+      ︙
+    </IonButton>
+
+      </IonItem>
+      
+      <IonCardContent>
+        
+          <IonItem lines='none'>
+            
+              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" className='icon2'/>
+              <div  className='text-container2' >
+            <IonCardTitle class="ion-card-title2-tab3">Package Portofolio Website</IonCardTitle>
+            <IonCardSubtitle class="ion-card-subtitle-tab3">Computer Science</IonCardSubtitle>
+            
+              </div>
+              
+          </IonItem>
+      
+        </IonCardContent>
+          <IonItem lines="none" className="order-section">
+      <div className="order-details">
+        <p>Total Order</p>
+        <h2>Rp 500.000</h2>
+      </div>
+
+      {/* Buttons */}
+      <div className="button-group">
+        <IonButton size="small" fill="outline" className='custom-button-outline'>Buy Again</IonButton>
+        <IonButton size="small" className='custom-button'>Review</IonButton>
+      </div>
+    </IonItem>
+        </IonCard>
+
       </IonContent>
     </IonPage>
   );
