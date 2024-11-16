8090b5e702cc9fcfe6c2e17dfca6c8df43d1d365
aileenliexiuai <aileen.liexiuai@gmail.com>
2024-11-17 01:30:18 +0700

    register tampilan responsive bener

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -2,15 +2,9 @@
 import React, { useState } from 'react';
 import {
   IonPage,
-  IonHeader,
   IonContent,
-  IonTitle,
-  IonToolbar,
-  IonInput,
   IonButton,
-  IonLabel,
-  IonButtons,
-  IonBackButton,
+
   IonCardContent,
   IonCardHeader
 } from '@ionic/react';
@@ -18,6 +12,9 @@ import { register } from '../api.service'; // Import your API service
 import { AppBar, Toolbar, IconButton, Card, CardContent, Typography, Box, Button, TextField, Checkbox, FormControlLabel } from '@mui/material';
 import GoogleIcon from '@mui/icons-material/Google'; // Import icon Google
 import { useHistory } from 'react-router-dom'; // Import useHistory for routing
+import { CssBaseline, GlobalStyles } from '@mui/material';
+import '@fontsource/poppins';  // Import the font
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const RegisterPage: React.FC = () => {
   const [data, setData] = useState({ name: '', email: '', password: '', confirmPassword: '' });
@@ -37,6 +34,23 @@ const RegisterPage: React.FC = () => {
   const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTermsAccepted(e.target.checked);
   };
+
+  const globalStyles = {
+    '*': {
+      fontFamily: 'Poppins',
+      margin: 0,
+      padding: 0,
+      boxSizing: 'border-box',
+    },
+    ':root': {
+      '--ion-font-family': 'Poppins',
+    },
+  };
+  const theme = createTheme({
+    typography: {
+      fontFamily: '"Poppins"',  // Set font di tema
+    },
+  });
 
   // Handle registration action
   const doRegister = async (e: React.FormEvent) => {
@@ -62,196 +76,209 @@ const RegisterPage: React.FC = () => {
   };
 
   return (
-    <IonPage>
-      <AppBar
-        position="fixed"
-        style={{
-          backgroundColor: 'white',
-          borderBottomLeftRadius: '30px',
-          borderBottomRightRadius: '30px',
-          height: '82px',
-          paddingTop: '25px'
-        }}
-      >
-        <Toolbar style={{ height: '100%', paddingBottom: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Box>
-            <IonButton fill="clear" routerLink="/register" className="text-mg text-gray-500">
-              Register
-            </IonButton>
-          </Box>
-          <div style={{ marginLeft: 'auto' }}>
-            <IonButton fill="clear" routerLink="/login" className="text-mg text-blue-500">
-              Login
-            </IonButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <IonContent className="ion-padding">
-
-      <IonCardHeader></IonCardHeader>
-          <IonCardContent style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '150px'}}>
-            {/* Membuat logo lebih kecil dan posisinya terpusat */}
-            <img alt="SkillX Logo" src="public/SkillXLogo.png" style={{ width: '250px', height: 'auto' }} />
-          </IonCardContent>
-
-          <div className="register-container max-w-sm mx-auto">
-            <form onSubmit={doRegister} className="space-y-5">
-              {/* Name Input Field */}
-              <div className="mb-5 flex justify-center">
-                <TextField
-                  name="name"
-                  label="Your Name"
-                  type="text"
-                  placeholder="John Doe"
-                  value={data.name}
-                  onChange={handleInputChange}
-                  fullWidth
-                  variant="outlined"
-                  required
-                  size="medium"
-                  sx={{
-                    maxWidth: '80%',
-                    marginBottom: '10px',
-                    '& .MuiOutlinedInput-root': {
-                      borderRadius: '10px',
-                    },
-                  }}
-                />
-              </div>
-
-              {/* Email Input Field */}
-              <div className="mb-5 flex justify-center">
-                <TextField
-                  name="email"
-                  label="Your email"
-                  type="email"
-                  placeholder="name@company.com"
-                  value={data.email}
-                  onChange={handleInputChange}
-                  fullWidth
-                  variant="outlined"
-                  required
-                  size="medium"
-                  sx={{
-                    maxWidth: '80%',
-                    marginBottom: '10px',
-                    '& .MuiOutlinedInput-root': {
-                      borderRadius: '10px',
-                    },
-                  }}
-                />
-              </div>
-
-              {/* Password Input Field */}
-              <div className="mb-5 flex justify-center">
-                <TextField
-                  name="password"
-                  label="Password"
-                  type="password"
-                  placeholder="••••••••"
-                  value={data.password}
-                  onChange={handleInputChange}
-                  fullWidth
-                  variant="outlined"
-                  required
-                  size="medium"
-                  sx={{
-                    maxWidth: '80%',
-                    marginBottom: '10px',
-                    '& .MuiOutlinedInput-root': {
-                      borderRadius: '10px',
-                    },
-                  }}
-                />
-              </div>
-
-              {/* Confirm Password Input Field */}
-              <div className="mb-5 flex justify-center">
-                <TextField
-                  name="confirmPassword"
-                  label="Confirm Password"
-                  type="password"
-                  placeholder="••••••••"
-                  value={data.confirmPassword}
-                  onChange={handleInputChange}
-                  fullWidth
-                  variant="outlined"
-                  required
-                  size="medium"
-                  sx={{
-                    maxWidth: '80%',
-                    marginBottom: '10px',
-                    '& .MuiOutlinedInput-root': {
-                      borderRadius: '10px',
-                    },
-                  }}
-                />
-              </div>
-
-              {/* Terms and Conditions Checkbox */}
-              <div className="flex justify-center items-center mb-5">
-                <div style={{ maxWidth: '80%', width: '100%' }}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={termsAccepted}
-                        onChange={handleCheckboxChange}
-                        color="primary"
-                        required
-                      />
-                    }
-                    label={
-                      <span>
-                        I agree with the{' '}
-                        <a href="#" className="text-blue-600 hover:underline">
-                          terms and conditions
-                        </a>
-                      </span>
-                    }
-                    sx={{
-                      width: '100%',
-                      display: 'flex',
-                      justifyContent: 'flex-start',
-                    }}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <IonPage>
+        <AppBar
+          position="fixed"
+          style={{
+            backgroundColor: 'white',
+            borderBottomLeftRadius: '30px',
+            borderBottomRightRadius: '30px',
+            height: '82px',
+            paddingTop: '25px',
+          }}
+        >
+          <Toolbar
+            style={{
+              height: '100%',
+              paddingBottom: '10px',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <Box>
+              <IonButton fill="clear" routerLink="/register" className="text-mg text-gray-500">
+                Register
+              </IonButton>
+            </Box>
+            <div style={{ marginLeft: 'auto' }}>
+              <IonButton fill="clear" routerLink="/login" className="text-mg text-blue-500">
+                Login
+              </IonButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <IonContent className="ion-padding">
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              height: '100vh',
+              textAlign: 'center',
+            }}
+          >
+            {/* Logo */}
+            <img
+              alt="SkillX Logo"
+              src="public/SkillXLogo.png"
+              style={{
+                width: '250px',
+                height: 'auto',
+                marginBottom: '20px',
+              }}
+            />
+  
+            {/* Form */}
+            <form
+              onSubmit={doRegister}
+              style={{
+                width: '80%',
+                maxWidth: '400px',
+              }}
+            >
+              {/* Name Input */}
+              <TextField
+                name="name"
+                label="Your Name"
+                type="text"
+                placeholder="John Doe"
+                value={data.name}
+                onChange={handleInputChange}
+                fullWidth
+                variant="outlined"
+                required
+                size="medium"
+                sx={{
+                  marginBottom: '10px',
+                  '& .MuiOutlinedInput-root': {
+                    borderRadius: '10px',
+                  },
+                }}
+              />
+  
+              {/* Email Input */}
+              <TextField
+                name="email"
+                label="Your Email"
+                type="email"
+                placeholder="name@company.com"
+                value={data.email}
+                onChange={handleInputChange}
+                fullWidth
+                variant="outlined"
+                required
+                size="medium"
+                sx={{
+                  marginBottom: '10px',
+                  '& .MuiOutlinedInput-root': {
+                    borderRadius: '10px',
+                  },
+                }}
+              />
+  
+              {/* Password Input */}
+              <TextField
+                name="password"
+                label="Password"
+                type="password"
+                placeholder="••••••••"
+                value={data.password}
+                onChange={handleInputChange}
+                fullWidth
+                variant="outlined"
+                required
+                size="medium"
+                sx={{
+                  marginBottom: '10px',
+                  '& .MuiOutlinedInput-root': {
+                    borderRadius: '10px',
+                  },
+                }}
+              />
+  
+              {/* Confirm Password Input */}
+              <TextField
+                name="confirmPassword"
+                label="Confirm Password"
+                type="password"
+                placeholder="••••••••"
+                value={data.confirmPassword}
+                onChange={handleInputChange}
+                fullWidth
+                variant="outlined"
+                required
+                size="medium"
+                sx={{
+                  marginBottom: '10px',
+                  '& .MuiOutlinedInput-root': {
+                    borderRadius: '10px',
+                  },
+                }}
+              />
+  
+              {/* Terms and Conditions */}
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={termsAccepted}
+                    onChange={handleCheckboxChange}
+                    color="primary"
+                    required
+                    sx={{alignSelf: 'flex-start' }} // Jarak antara checkbox dan label
                   />
-                </div>
-              </div>
+                }
+                label={
+                  <Typography
+                    sx={{
+                      color: '#4B4B4B',
+                      fontSize: {
+                        xs: '0.65rem', // For extra small screens (mobile)
+                        sm: '0.85rem', // For small screens
+                        md: '1rem',  // For medium screens and above
+                      },
+                      display: 'inline', // Ensures that the label and asterisk are inline
+
+                    }}
+                  >
+                    I agree with the{' '}
+                    <a href="#" className="text-blue-600 hover:underline">
+                      terms and conditions
+                    </a>
+                  </Typography>
+                }
+                sx={{
+                  display: 'flex',
+                }}
+              />
 
               {/* Register Button */}
-              <div className="flex justify-center items-center mb-5">
-                <Button
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                  size="large"
-                  sx={{
-                    maxWidth: '80%',
-                    borderRadius: '10px',
-                    paddingY: '12px',
-                    fontSize: '1.1rem',
-                  }}
-                >
-                  Register
-                </Button>
-              </div>
-
-              {/* Display response if registration is successful */}
-              {resp && (
-                <div className="mt-5 text-green-500">
-                  Registrasi berhasil: {JSON.stringify(resp)}
-                </div>
-              )}
-
-                {/* Google Login Button */}
-            <div className="flex justify-center items-center mb-5">
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+                size="large"
+                sx={{
+                  borderRadius: '10px',
+                  paddingY: '12px',
+                  fontSize: '1.1rem',
+                  marginBottom: '16px',
+                }}
+              >
+                Register
+              </Button>
+  
+              {/* Google Login Button */}
               <Button
                 variant="outlined"
                 fullWidth
                 size="large"
                 sx={{
                   marginTop: '10px',
-                  maxWidth: '80%',
                   borderRadius: '10px',
                   paddingY: '12px',
                   fontSize: '1.1rem',
@@ -270,12 +297,11 @@ const RegisterPage: React.FC = () => {
               >
                 Google
               </Button>
-            </div>
-
             </form>
           </div>
-      </IonContent>
-    </IonPage>
+        </IonContent>
+      </IonPage>
+    </ThemeProvider>
   );
 };
 
